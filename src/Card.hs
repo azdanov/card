edfc0caa9431f9d8af19c094597a292ed9dd8822
Anton Zdanov <azdanov@pm.me>
2018-09-07 13:26:32 +0300

    chore: remove redundant comments

--- src/Card.hs
+++ src/Card.hs
@@ -6,18 +6,12 @@ module Card where
 
 import           Data.Char
 
--- ===================================
-
 toDigits :: Integer -> [Integer]
 toDigits x | x < 0     = error "Negative number provided"
            | otherwise = map (toInteger . digitToInt) $ show x
 
--- ===================================
-
 toDigitsRev :: Integer -> [Integer]
 toDigitsRev = reverse . toDigits
-
--- ===================================
 
 doubleSecond :: [Integer] -> [Integer]
 doubleSecond xs = zipWith f xs [0 ..]
@@ -25,18 +19,12 @@ doubleSecond xs = zipWith f xs [0 ..]
   f x i | even i    = x
         | otherwise = x * 2
 
--- ===================================
-
 sumDigits :: [Integer] -> Integer
 sumDigits = sum . concatMap toDigits
-
--- ===================================
 
 isValid :: Integer -> Bool
 isValid = f . sumDigits . doubleSecond . toDigitsRev
   where f x = x `mod` 10 == 0
-
--- ===================================
 
 numValid :: [Integer] -> Integer
 numValid = sum . map (const 1) . filter isValid
