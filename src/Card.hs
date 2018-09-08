7e188c08c26c1631decd92057605fc2117cfe517
Jannis Limperg <jannis@limperg.de>
2018-09-08 19:23:10 +0200

    Handle error with Maybe instead of `error`
    This eliminates a call to `error` in `Card.toDigits`. `toDigits` now
    returns a `Maybe` instead.

--- src/Card.hs
+++ src/Card.hs
@@ -6,12 +6,9 @@ module Card where
 
 import           Data.Char
 
-toDigits :: Integer -> [Integer]
-toDigits x | x < 0     = error "Negative number provided"
-           | otherwise = map (toInteger . digitToInt) $ show x
-
-toDigitsRev :: Integer -> [Integer]
-toDigitsRev = reverse . toDigits
+toDigits :: Integer -> Maybe [Integer]
+toDigits x | x < 0     = Nothing
+           | otherwise = Just $ map (toInteger . digitToInt) $ show x
 
 doubleSecond :: [Integer] -> [Integer]
 doubleSecond xs = zipWith f xs [0 ..]
@@ -19,13 +16,15 @@ doubleSecond xs = zipWith f xs [0 ..]
   f x i | even i    = x
         | otherwise = x * 2
 
-sumDigits :: [Integer] -> Integer
-sumDigits = sum . concatMap toDigits
-
 isValid :: Integer -> Bool
-isValid = f . sumDigits . doubleSecond . toDigitsRev
+isValid n = case toDigits n of
+  Nothing -> False
+  Just digits -> f $ sum $ doubleSecond $ reverse digits
   where f x = x `mod` 10 == 0
+  -- Here we consume the `Maybe [Integer]` produced by `toDigits`. To do so, we
+  -- must distinguish between two cases: Either `toDigits` failed, producing
+  -- `Nothing`, or it succeeded, producing `Just` a list of digits. Since
+  -- failure means that the number is invalid, we return `False` in that case.
 
 numValid :: [Integer] -> Integer
 numValid = toInteger . length . filter isValid
-
